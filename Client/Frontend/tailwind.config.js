2d4e631180ddeae6baccd443a1af24b1f2e060f5
Afshan Hashemi Hosseinabad <afsha719@student.liu.se>
2024-10-13 15:29:01 +0200

    Add tailwind colours

--- Client/Frontend/tailwind.config.js
+++ Client/Frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        NavyBlue: '#0A3D62',
+        SlateBlue: '#2E5984',
+        LightGray: '#D1D5DB',
+        SoftBlue: '#A3C3D9',
+        TealBlue: '#237F94',
+        BG: '#F5F7FA',
+        ButtonsBlue: '#0650C6',
+        AlaramRed: '#C60606',
+      },
+    },
   },
   plugins: [],
 }
