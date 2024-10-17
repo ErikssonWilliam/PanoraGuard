e26022cf3f7d3fe93b5cc6f8e715aae341c78518
Afshan Hashemi Hosseinabad <afsha719@student.liu.se>
2024-10-17 13:53:17 +0200

    Update profile.jsx

--- Client/Frontend/src/components/profile.jsx
+++ Client/Frontend/src/components/profile.jsx
@@ -16,7 +16,7 @@ const ProfilePage = () => {
                 {/* User Information Section */}
                 <div className="userInfoSection flex-1 flex bg-white rounded-lg mx-10 mt-4 relative">
                     {/* Left Side - Blue Part of User Info */}
-                    <div className="blueSection w-1/4 bg-NavyBlue p-2 flex flex-col justify-center items-start relative"> 
+                    <div className="blueSection w-1/4 bg-NavyBlue p-2 flex flex-col justify-center items-start relative rounded-tl-lg rounded-bl-lg"> 
                         <div className="profilePicture w-48 h-48 bg-gray-300 rounded-full overflow-hidden absolute top-1/2 transform -translate-y-1/2"> 
                             <img 
                                 src={profileImage} 
@@ -27,7 +27,7 @@ const ProfilePage = () => {
                     </div>
 
                     {/* Right Side - White Part of User Info */}
-                    <div className="whiteSection w-3/4 bg-LightGray p-6 flex flex-col relative">
+                    <div className="whiteSection w-3/4 bg-LightGray p-6 flex flex-col relative rounded-tr-lg rounded-br-lg">
                         {/* Greeting and Name Section */}
                         <div className="greeting absolute top-6 left-6">
                             <h2 className="text-3xl font-bold text-blue-600">Hello John,</h2>
@@ -35,7 +35,7 @@ const ProfilePage = () => {
                         </div>
 
                         {/* Description Section */}
-                        <div className="description flex-1 flex items-start justify-start mt-24"> {/* Adjusted margin-top for better spacing */}
+                        <div className="description flex-1 flex items-start justify-start mt-24">
                             <div className="max-w-md w-full">
                                 <p className="text-gray-700">
                                     About: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam molestie, justo nec auctor facilisis, lectus metus venenatis nulla, dignissim aliquam turpis odio tempus ante.
@@ -51,11 +51,11 @@ const ProfilePage = () => {
                     <h2 className="text-lg font-semibold">Change Password</h2>
                     <div className="mt-2">
                         <label className="block text-gray-700">New Password</label>
-                        <input type="password" className="passwordInput border border-gray-300 rounded-lg p-2 w-full mt-1" placeholder="Enter new password" />
+                        <input type="password" className="mt-1 block w-full px-4 py-2 border border-ButtonsBlue rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="Enter new password" />
                     </div>
                     <div className="mt-4">
                         <label className="block text-gray-700">Repeat Password</label>
-                        <input type="password" className="passwordInput border border-gray-300 rounded-lg p-2 w-full mt-1" placeholder="Repeat new password" />
+                        <input type="password" className="mt-1 block w-full px-4 py-2 border border-ButtonsBlue rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:text-sm" placeholder="Repeat new password" />
                     </div>
                     <button className="submitButton mt-4 bg-blue-600 text-white rounded-lg p-2 w-full">Submit</button>
                 </div>
