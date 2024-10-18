1d013ef84250f762171738b9c8b0d4c9767ebed4
Johan Lovén <johlo422@student.liu.se>
2024-10-18 11:00:52 +0200

    Combined enable and disable ACAP to one function

--- Server/LAN/app/testRoutes.py
+++ Server/LAN/app/testRoutes.py
@@ -3,7 +3,6 @@ from .models import *
 import requests
 import jwt
 from requests.auth import HTTPBasicAuth
-from apscheduler.schedulers.background import BackgroundScheduler
 
 api = Blueprint("api", __name__)
 
@@ -149,67 +148,7 @@ def test_jwt_works_in_both_servers():
     if auth_header and auth_header.startswith("Bearer "):
         token = auth_header.split(" ")[1]
         return jwt.decode(token, "your_random_secret_text", algorithms="HS256")
-
-# Function to enable ACAP
-def enable_acap(url):
-    
-    try:
-        response = requests.post(url, auth=HTTPBasicAuth(username, password))
-        # response = requests.post(url, data=client_data, auth=HTTPBasicAuth(username, password), stream=True)
-    
-        if response.status_code == 200:
-            return {"status": "success", "message": f"ACAP enabled successfully"}
-        else:
-            return {"status": "failed", "error": response.text}
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
-        return {"status": "failed", "error": str(e)}
-
-
-# Function to disable ACAP
-def disable_acap(url):
-
-    try:
-        response = requests.post(url, auth=HTTPBasicAuth(username, password))
-    
-        if response.status_code == 200:
-            return {"status": "success", "message": f"ACAP disabled successfully"}
-        else:
-            return {"status": "failed", "error": response.text}
-    except requests.exceptions.RequestException as e:
-        print(f"Request failed: {e}")
-        return {"status": "failed", "error": str(e)}   
-
-
-
-
-@api.route("/schedule-acap", methods=["GET"])
-def schedule_acap():
-    # Extract JSON data from the incoming request
-    #client_data = request.get_json()
-
-    # Camera details
-    camera_ip = "192.168.1.121"
-    acap_name = "consolidated_jansson"
-
-    
-    # Define the external URL
-    url = f"http://{camera_ip}/axis-cgi/applications/control.cgi?action=stop&package={acap_name}"
-
-    
-    try:
-
-        #result = disable_acap(url)
-
-        scheduler = BackgroundScheduler()
-        scheduler.add_job(enable_acap, 'cron', hour=11, minute=37, args=[url])  
-        scheduler.add_job(disable_acap, 'cron', hour=11, minute=38, args=[url])  
-        scheduler.start()
-
-
-        return jsonify({"status": "success", "message": f"ACAP scheduling set for {acap_name} from 11:00 to 11:05 daily"}), 200
-    except Exception as e:
-        return jsonify({"status": "failed", "error": str(e)}), 500   
+  
 
 
 
