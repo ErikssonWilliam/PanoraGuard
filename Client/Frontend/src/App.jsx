18bd0510b12c8c5c7ba9bea4857e268cf530b0bc
Maliha Tunzira <33031428+Tunzir12@users.noreply.github.com>
2024-10-29 21:25:29 +0100

    added react-router-dom

--- Client/Frontend/src/App.jsx
+++ Client/Frontend/src/App.jsx
@@ -4,15 +4,18 @@ import Admin from './pages/admin'
 import PanoraGuardDashboard from './components/PanoraGuardDashboard';
 import ProfilePage from './components/profile';
 import Login from './components/login';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-
-      <Login />
-      < PanoraGuardDashboard />
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>} />
+      <Route path='/profile' element={<ProfilePage />} />
+      <Route path='/dashboard' element={<PanoraGuardDashboard />} />
+      <Route path='/admin' element={<Admin />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 export default App
