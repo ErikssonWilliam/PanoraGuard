68048dca2e2f704d01336c1920ccfae0394bcad0
Axel Strid <axelstrid@macbook-pro-3.students.netlogon.liu.se>
2024-11-05 11:03:41 +0100

    Add new alarm logic in external server

--- Server/External/app/alarms/alarms_controller.py
+++ Server/External/app/alarms/alarms_controller.py
@@ -2,7 +2,7 @@ from app.models import *
 from flask import jsonify
 from .alarms_service import AlarmService
 
-# will request entered data, tries the calls and returns the results
+# Will request entered data, tries the calls and returns the results
 
 
 class AlarmController:
@@ -11,7 +11,9 @@ class AlarmController:
         return jsonify[({"alarmID" : a.alarm.id, "timestamp" : a.alarm.timestamp} for a in alarms)]
 
     def add_alarm():
-        return AlarmService.add_alarm()
+        alarm_data = request.get_json()
+        new_alarm = AlarmService.create_alarm(alarm_data)
+        return jsonify(new_alarm), 201
 
     def get_alarm_by_id(alarm_id):
         #        alarm = Alarm.query.get(alarm_id)
--- Server/External/app/alarms/alarms_routes.py
+++ Server/External/app/alarms/alarms_routes.py
@@ -3,22 +3,27 @@ from .alarms_controller import AlarmController
 
 alarms_bp = Blueprint("alarms", __name__)
 
-
+# Get all alarms
 @alarms_bp.route("/", methods=["GET"])
 def get_alarms():
     return AlarmController.get_alarms()
 
+# Get latest alarm
+@alarms_bp.route("/new", methods=["GET"])
+def get_new_alarm():
+    return AlarmController.get_new_alarm()
 
+# Add alarm
 @alarms_bp.route("/add", methods=["POST"])
-def AlarmController():
+def add_alarm():
     return AlarmController.add_alarm()
 
-
+# Get alarm by id
 @alarms_bp.route("/<string:alarm_id>", methods=["GET"])
 def get_alarm_by_id(alarm_id):
     return AlarmController.get_alarm_by_id(alarm_id)
 
-
+# Delete alarm by id
 @alarms_bp.route("/<string:alarm_id>", methods=["DELETE"])
 def delete_alarm_by_id(alarm_id):
     return AlarmController.delete_alarm_by_id(alarm_id)
