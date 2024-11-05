bf37ee02bd8dc2fc19b922cdb612dab92a1cba56
Jonhe899 <122796833+Jonhe899@users.noreply.github.com>
2024-11-05 20:20:48 +0100

    formatting

--- Server/External/app/__init__.py
+++ Server/External/app/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from .extensions import bcrypt, db
 from .routes import init_routes
-#from .mock_data import create_mock_data
+
+# from .mock_data import create_mock_data
 from flask_cors import CORS
 
 
@@ -21,11 +22,11 @@ def create_app():
     # Initialize the database
     db.init_app(app)
     with app.app_context():
-     #   db.drop_all()
+        #   db.drop_all()
         # Create all tables defined in the models
         db.create_all()
         # Fill tables with mock data
-     #   create_mock_data()
+    #   create_mock_data()
 
     init_routes(app)
 
