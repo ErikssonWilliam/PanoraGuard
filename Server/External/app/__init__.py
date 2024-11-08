2c3141511231ada7beda046defaef3a4c51fc65e
snek-9 <jontehermansson@hotmail.com>
2024-11-08 14:14:34 +0100

    connecting admins addUser form to submit a POST request to add a new user in the database

--- Server/External/app/__init__.py
+++ Server/External/app/__init__.py
@@ -23,11 +23,11 @@ def create_app():
     db.init_app(app)
     with app.app_context():
         print("Creating all tables")
-        db.drop_all()
+    #    db.drop_all()
         # Create all tables defined in the models
         db.create_all()
         # Fill tables with mock data
-        create_mock_data()
+    #    create_mock_data()
 
     init_routes(app)
 
