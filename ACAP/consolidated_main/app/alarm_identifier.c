18d15d2054b74d3d6b88d621bc18abd96ccabbcc
Martin Hallbäck <martin.hallback@perimed-instruments.com>
2024-11-14 10:41:19 +0100

    fixed bug

--- ACAP/consolidated_main/app/alarm_identifier.c
+++ ACAP/consolidated_main/app/alarm_identifier.c
@@ -14,9 +14,11 @@
 #include <curl/curl.h> // For HTTP requests
 
 // Define constants
-#define EXTERNAL_URL "http://192.168.1.135:5000/processing/camera/data_JSON" // For external server
+#define CAMERA_ID "B8A44F9EEE36" //Serial number for camera ip 121
+//#define CAMERA_ID "B8A44F9EEFE0" //Serial nummber for camera ip 116
+#define EXTERNAL_URL "http://192.168.1.145:5000/alarms/add" // For external server
 #define ENABLE_SNAPSHOT_URL "http://192.168.1.121/config/rest/best-snapshot/v1/enabled" // For camera api endpoint
-#define CAMERA_ID_URL "http://192.168.1.121/axis-cgi/basicdeviceinfo.cgi"
+
 // -----------------------------------------
 
 // Structure to hold channel topic and source info
@@ -34,30 +36,15 @@ static void on_connection_error(mdb_error_t *error, void *user_data)
 }
 
 // Code ------------------------------------------------------------------
+static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    (void)userp;
 
-typedef struct {
-    char *memory;
-    size_t size;
-} response_data_t;
-
-// Function to handle the response from HTTP requests
-static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp) {
     size_t total_size = size * nmemb;
-    response_data_t *mem = (response_data_t *)userp;
-
-    char *ptr = realloc(mem->memory, mem->size + total_size + 1);
-    if (ptr == NULL) {
-        syslog(LOG_ERR, "Not enough memory for response");
-        return 0;
-    }
-
-    mem->memory = ptr;
-    memcpy(&(mem->memory[mem->size]), contents, total_size);
-    mem->size += total_size;
-    mem->memory[mem->size] = 0;
-
+    syslog(LOG_INFO, "Response from camera: %.*s", (int)total_size, (char *)contents);
     return total_size;
 }
+
 
 static void post_to_external(const char *data)
 {
@@ -83,66 +70,6 @@ static void post_to_external(const char *data)
     }
 }
 
-static bool extract_camera_id(const char *json_data, char **camera_id)
-{
-    json_error_t error;
-    json_t *root = json_loads(json_data, 0, &error);
-    if (!root) {
-        syslog(LOG_ERR, "JSON parsing error in camera ID: %s", error.text);
-        return false;
-    }
-
-    json_t *id = json_object_get(json_object_get(json_object_get(root, "data"), "propertyList"), "SerialNumber");
-    if (json_is_string(id)) {
-        *camera_id = strdup(json_string_value(id));
-        json_decref(root);
-        return true;
-    }
-
-    syslog(LOG_ERR, "SerialNumber not found or invalid in JSON response.");
-    json_decref(root);
-    return false;
-}
-
-static void get_camera_id(char **camera_id)
-{
-    CURL *curl = curl_easy_init();
-    if (!curl) {
-        syslog(LOG_ERR, "Failed to initialize CURL");
-        return;
-    }
-
-    response_data_t response_data = {malloc(1), 0}; // Allocate initial memory
-    if (!response_data.memory) {
-        syslog(LOG_ERR, "Memory allocation failed for response_data");
-        curl_easy_cleanup(curl);
-        return;
-    }
-
-    struct curl_slist *headers = curl_slist_append(NULL, "Accept: application/json");
-    headers = curl_slist_append(headers, "Content-Type: application/json");
-
-    const char *data = "{\"apiVersion\":\"1.0\",\"context\":\"Client defined request ID\",\"method\":\"getAllUnrestrictedProperties\"}";
-
-    curl_easy_setopt(curl, CURLOPT_URL, CAMERA_ID_URL);
-    curl_easy_setopt(curl, CURLOPT_POST, 1L);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response_data);
-
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
-        syslog(LOG_ERR, "curl_easy_perform() failed: %s", curl_easy_strerror(res));
-    } else if (!extract_camera_id(response_data.memory, camera_id)) {
-        syslog(LOG_ERR, "Camera ID extraction failed");
-    }
-
-    curl_easy_cleanup(curl);
-    curl_slist_free_all(headers);
-    free(response_data.memory);
-}
-
 static bool parse_class_data(const json_t *first_class, char **type_value, double *score_value)
 {
     json_t *score = json_object_get(first_class, "score");
@@ -161,22 +88,20 @@ static bool parse_class_data(const json_t *first_class, char **type_value, doubl
     return true;
 }
 
-static bool parse_image_data(const json_t *image, char **image_value, char **time)
+static bool parse_image_data(const json_t *image, char **image_value)
 {
     json_t *data = json_object_get(image, "data");
-    json_t *timestamp = json_object_get(image, "timestamp");
 
-    if (!json_is_string(data) || !json_is_string(timestamp)) {
-        syslog(LOG_ERR, "Missing image data or timestamp.");
+    if (!json_is_string(data)) {
+        syslog(LOG_ERR, "Missing image data.");
         return false;
     }
 
     *image_value = strdup(json_string_value(data));
-    *time = strdup(json_string_value(timestamp));
     return true;
 }
 
-static bool parse_json_payload(const mdb_message_payload_t *payload, char **type_value, double *score_value, char **image_value, char **time)
+static bool parse_json_payload(const mdb_message_payload_t *payload, char **type_value, double *score_value, char **image_value)
 {
     json_error_t error;
     json_t *root = json_loadb((const char *)payload->data, payload->size, 0, &error);
@@ -199,7 +124,7 @@ static bool parse_json_payload(const mdb_message_payload_t *payload, char **type
     }
 
     json_t *image = json_object_get(root, "image");
-    if (!json_is_object(image) || !parse_image_data(image, image_value, time)) {
+    if (!json_is_object(image) || !parse_image_data(image, image_value)) {
         syslog(LOG_ERR, "Failed to parse image data.");
         json_decref(root);
         return false;
@@ -218,10 +143,8 @@ static void on_message(const mdb_message_t *message, void *user_data)
     char *type_value = NULL;
     double score_value = 0.0;
     char *image_value = NULL;
-    char *time = NULL;
-    char *camera_id = NULL;
 
-    if (!parse_json_payload(payload, &type_value, &score_value, &image_value, &time)) {
+    if (!parse_json_payload(payload, &type_value, &score_value, &image_value)) {
         return;
     }
 
@@ -229,34 +152,22 @@ static void on_message(const mdb_message_t *message, void *user_data)
         syslog(LOG_INFO, "Detected object of type: %s, no alarm will be raised", type_value);
         free(type_value);
         free(image_value); 
-        free(time);
         return;
     }
-
-    get_camera_id(&camera_id);
-    if (!camera_id) {
-        free(type_value);
-        free(image_value); 
-        free(time);
-        return;
-    }
-
     // Log the detected object information
     syslog(LOG_INFO,
-           "Detected object - Topic: %s, Source: %s, Time: %s, Type: %s, Score: %.4f, Image Data: %s, Camera ID: %s",
+           "Detected object - Topic: %s, Source: %s, Type: %s, Score: %.4f, Image Data: %s, Camera ID: %s",
            channel_identifier->topic,
            channel_identifier->source,
-           time,
            type_value,
            score_value,
            image_value,
-           camera_id);
+           CAMERA_ID);
     
     json_t *json_data = json_pack("{s:f, s:s, s:s, s:s}",
                                   "confidence_score", score_value,
                                   "image_base64", image_value,
-                                  "timestamp", time,
-                                  "camera_id", camera_id,
+                                  "camera_id", CAMERA_ID,
                                   "type", type_value);
 
 
@@ -266,7 +177,6 @@ static void on_message(const mdb_message_t *message, void *user_data)
     json_decref(json_data);
     free(type_value);
     free(image_value);
-    free(time);
     
 }
 
