8cf996149d1472482da6853533d4ee71f0b000c6
Afshan Hashemi Hosseinabad <afsha719@student.liu.se>
2024-11-16 16:41:49 +0100

    Update profile.jsx

--- Client/Frontend/src/components/profile.jsx
+++ Client/Frontend/src/components/profile.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import profileImage from "../assets/C3WBG.png";
 import bellIcon from "../assets/bell-01.png";
-import { Link } from "react-router-dom";
 import { baseURL } from "../api/axiosConfig";
 import { useNavigate } from "react-router-dom";
 
