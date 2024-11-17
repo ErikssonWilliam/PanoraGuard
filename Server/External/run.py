97e6045bbecfe6d7d7527918b1804526df9c978d
olosw720 <olosw720@student.liu.se>
2024-11-17 15:56:06 +0100

    Solved small error

--- Server/External/run.py
+++ Server/External/run.py
@@ -5,8 +5,8 @@ from app.socketio_instance import socketio
 
 app = create_app()
 if __name__ == "__main__":
-    # app.run(debug=True, host="0.0.0.0", port=5000)
-    migrate()
-    upgrade()
-    create_mock_data()
+    with app.app_context():
+        migrate()
+        upgrade()
+        create_mock_data()
     socketio.run(app, host="0.0.0.0", port=5000)
