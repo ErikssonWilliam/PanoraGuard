7a93f8083465d7774ed42a9a4a3adc72354aa979
spacemen0 <frakctures@outlook.com>
2024-11-20 11:31:05 +0100

    push fixes

--- Server/External/run.py
+++ Server/External/run.py
@@ -1,4 +1,4 @@
-from flask_migrate import upgrade
+from flask_migrate import upgrade, init,migrate
 from app import create_app
 from app.socketio_instance import socketio
 from app.mock_data import create_mock_data
@@ -6,6 +6,11 @@ from app.mock_data import create_mock_data
 app = create_app()
 if __name__ == "__main__":
     with app.app_context():
+        try:
+            init()
+        except SystemExit as e:
+            pass
+        migrate()
         upgrade()
         create_mock_data()
     socketio.run(app, host="0.0.0.0", port=5000)
