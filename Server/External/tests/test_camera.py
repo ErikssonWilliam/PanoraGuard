a8e0d0f2afc1b80c82d44f23bda283e89ff0f397
Emilia <emiax111@studentliu.se>
2024-11-21 11:27:14 +0100

    mer fix i test camera

--- Server/External/tests/test_camera.py
+++ Server/External/tests/test_camera.py
@@ -2,13 +2,10 @@ import pytest
 from app.models import Camera
 from app.cameras.cameras_service import CameraService
 
+def test_get_cameras(session):
 
-@pytest.fixture
-def test_camera(session):
-    session.query(Camera).delete()
-    session.commit()
     cameras1 = Camera(
-        id="2002",
+        id="2001",
         ip_address="192.168.1.1",
         location="Test Location",
         confidence_threshold=85,
@@ -22,25 +19,22 @@ def test_camera(session):
     session.add(cameras1)
     session.add(cameras2)
     session.commit()
-
-
-@pytest.fixture
-def test_get_cameras(session, test_camera):
-    # Retrieve cameras using the service method
     cameras = CameraService.get_cameras()
 
-    # Assert that we get the correct data in the expected format
     assert isinstance(cameras, list)
     assert len(cameras) == 2
-    assert cameras[0]["id"] == "2002"
+    assert cameras[0]["id"] == "2001"
     assert cameras[0]["ip_address"] == "192.168.1.1"
     assert cameras[0]["location"] == "Test Location"
-    assert cameras[0]["confidence_threshold"] == 0.85
+    assert cameras[0]["confidence_threshold"] == 85.0
 
     assert cameras[1]["id"] == "1998"
     assert cameras[1]["ip_address"] == "192.168.2.2"
     assert cameras[1]["location"] == "Warehouse"
-    assert cameras[1]["confidence_threshold"] == 0.80
+    assert cameras[1]["confidence_threshold"] == 80.0
+
+    session.query(Camera).delete()
+    session.commit()
 
 
 def test_camera_by_id(session):
@@ -53,11 +47,9 @@ def test_camera_by_id(session):
     )
     session.add(cameras1)
     session.commit()
-    # Retrieve the camera by its ID
     camera = CameraService.get_camera_by_id(cameras1.id)
 
-    # Assert that we get the correct data in the expected format
-    # assert camera is not None
+    assert camera is not None
     assert camera["id"] == "2002"
     assert camera["ip_address"] == "192.168.1.1"
     assert camera["location"] == "Test Location"
