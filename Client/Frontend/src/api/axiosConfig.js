701936f2af5512bb98a3f9c544cdf96b71c62d03
Chang Guo <chagu825@student.liu.se>
2024-11-26 09:58:01 +0000

    solve merge conflicts

--- Client/Frontend/src/api/axiosConfig.js
+++ Client/Frontend/src/api/axiosConfig.js
@@ -1,8 +1,8 @@
 // src/api/axiosConfig.js
 import axios from "axios";
 
-const externalURL = "https://company3-externalserver.azurewebsites.net"; // URL to Azure Cloud Server
-//const externalURL = "http://127.0.0.1:5000"; // URL to local server
+//const baseURL = "https://company3-externalserver.azurewebsites.net"; // URL to Azure Cloud Server
+const externalURL = "https://company3-externalserver.azurewebsites.net"; // URL to local server
 const lanURL = "http://127.0.0.1:5100";
 
 const axiosInstance = axios.create({
