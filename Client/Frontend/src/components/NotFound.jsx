7d53b4975cca54512d386bfaf1a5e5c7bdf672bb
spacemen0 <frakctures@outlook.com>
2024-12-03 19:52:15 +0100

    format

--- Client/Frontend/src/components/NotFound.jsx
+++ Client/Frontend/src/components/NotFound.jsx
@@ -6,7 +6,9 @@ const NotFound = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-md text-center max-w-sm w-full">
-        <h1 className="text-2xl font-semibold text-gray-800 mb-4">404 - Page Not Found</h1>
+        <h1 className="text-2xl font-semibold text-gray-800 mb-4">
+          404 - Page Not Found
+        </h1>
         <p className="text-gray-600 mb-6">
           Oops! The page you are looking for doesn&apos;t exist.
         </p>
